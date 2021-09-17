fbe8716185da6d5af50b8ec1f0c1595912ef9c87
Erick Castille <ecastille924@gmail.com>
2021-09-17 19:58:41 -0400

    setting up index.html and index.js

---
+++ record_closet_client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-thunk'
+import thunk from 'react-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import App from './components/App'
+import {rootReducer} from './reducers/rootReducer'
