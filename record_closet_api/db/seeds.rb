8b05de12dc8d8b56162432a808ccb42e22d06487
Erick Castille <ecastille924@gmail.com>
2021-09-18 19:00:25 -0400

    creating seed data

--- record_closet_api/db/seeds.rb
+++ record_closet_api/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table "records", force: :cascade do |t|
+#     t.string "title"
+#     t.string "artist"
+#     t.string "condition"
+#     t.integer "year"
+#     t.integer "rating"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
+
+Record.create(title: "Easter is Cancelled", artist: "The Darkness", condition: "mint", year: 2020, rating: 5)
