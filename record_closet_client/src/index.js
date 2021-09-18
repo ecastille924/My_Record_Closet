470b9a53b3ceea9f9626fcaf13064c789395cc24
Erick Castille <ecastille924@gmail.com>
2021-09-17 20:02:46 -0400

    setting up store in index.js

--- record_closet_client/src/index.js
+++ record_closet_client/src/index.js
@@ -6,3 +6,15 @@ import thunk from 'react-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import App from './components/App'
 import {rootReducer} from './reducers/rootReducer'
+
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+    )
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById('root')
+    )
