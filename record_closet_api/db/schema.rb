6a3fbbeb34c5dec83e97e0a26d74a70d424f20ec
Erick Castille <ecastille924@gmail.com>
2021-09-21 19:16:16 -0400

    recreating user table with email, uid and provider for oauth

--- record_closet_api/db/schema.rb
+++ record_closet_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_231724) do
+ActiveRecord::Schema.define(version: 2021_09_21_231020) do
 
   create_table "records", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 2021_09_16_231724) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.string "password_digest"
+    t.string "uid"
+    t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
