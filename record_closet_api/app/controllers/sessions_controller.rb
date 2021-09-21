aa90d3b62abb5b52062c74dbfdd864d2d3409737
Erick Castille <ecastille924@gmail.com>
2021-09-21 19:37:34 -0400

    inserting notes about logic flow in sessions controller

--- record_closet_api/app/controllers/sessions_controller.rb
+++ record_closet_api/app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
 
 def create
+    #getting access token from google
     access_token = request.env["omniauth.auth"]
     user = User.create_from_omniauth(access_token)
+
+    #token authenticates rails request to google server
     user.google_token = access_token.credentials.access_token
+    
+    #access new token if first request
     refresh_token = access_token.credentials.refresh_token
     user.google_refresh_token = refresh_token if refresh_token.present?
     user.save!
 
+    #once new user is created, cookie is generated
     cookies.encrypted[current_user_id] = {value: user.id, expires: Time.now + 30.days}
 
     redirect_to root_path
