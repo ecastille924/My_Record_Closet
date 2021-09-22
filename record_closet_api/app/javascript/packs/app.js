72eaf3b19d185eda05dc16164d5ac600884ba851
Erick Castille <ecastille924@gmail.com>
2021-09-21 21:01:14 -0400

    removing files to try different user auth approach

--- record_closet_api/app/javascript/packs/app.js
+++
@@ -1,6 +0,0 @@
-import Home from "../../../../record_closet_client/src/components/Home";
-import WebPackerReact from 'webpacker-react'
-
-WebPackerReact.setup({
-    Home
-})
--- record_closet_client/src/components/Login.js
+++ record_closet_client/src/components/Login.js
@@ -7,7 +7,8 @@ function Login() {
             <br/>
             <input type= "text" placeholder="Username" className="form-inputs" /><br/>
             <input type= "text" placeholder="email" className="form-inputs" /><br/>
-            <input type= "password" placeholder="Password" className="form-inputs" /> 
+            <input type= "password" placeholder="Password" className="form-inputs" /><br/>
+            <input type="button" value="Login" /> {""} <input type="button" value="Logout" />
         </div>
     )
 }
