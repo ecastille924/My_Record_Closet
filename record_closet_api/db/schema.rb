b8f1881ea3ab8d0dc1a4a269465d3c67cdf9f632
Erick Castille <ecastille924@gmail.com>
2021-09-25 13:42:32 -0400

    removing unused columns from user table

--- record_closet_api/db/schema.rb
+++ record_closet_api/db/schema.rb
@@ -25,10 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_231020) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
-    t.string "uid"
-    t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
