22decceb30acb13248a46b60e0b5791219db8acc
Erick Castille <ecastille924@gmail.com>
2021-09-25 14:56:41 -0400

    updating users controller to allow user to access their data

--- record_closet_api/app/controllers/users_controller.rb
+++ record_closet_api/app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
     
       private
       def user_params
-        params.require(:user).permit(:username, :password, :bio, :avatar)
+        params.require(:user).permit(:username, :password)
       end
 end
