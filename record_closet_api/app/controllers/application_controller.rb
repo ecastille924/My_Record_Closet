79262f034815529aeaae41e45eec87a9e297753a
Erick Castille <ecastille924@gmail.com>
2021-09-26 15:10:51 -0400

    updating users and app controller logic

--- record_closet_api/app/controllers/application_controller.rb
+++ record_closet_api/app/controllers/application_controller.rb
@@ -1,37 +1,28 @@
 class ApplicationController < ActionController::Base
-    before_action :authorized
-  
-    def encode_token(payload)
-      JWT.encode(payload, 'my_s3cr3t')
+  skip_before_action :verify_authenticity_token
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
+      
+    def login!
+          session[:user_id] = @user.id
     end
-  
-    def auth_header
-      request.headers['Authorization']
-    end
-  
-    def decoded_token
-      if auth_header
-        token = auth_header.split(' ')[1]
-        begin
-          JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
-        rescue JWT::DecodeError
-          nil
-        end
-      end
-    end
-  
-    def current_user
-      if decoded_token
-        user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user_id)
-      end
-    end
-  
+    
     def logged_in?
-      !!current_user
+          !!session[:user_id]
     end
-  
-    def authorized
-      render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    
+    def current_user
+          @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    def authorized_user?
+          @user == current_user
+    end
+    
+    def logout!
+          session.clear
+    end
+    
+    def set_user
+        @user = User.find_by(id: session[:user_id])
     end
   end
--- record_closet_api/app/controllers/users_controller.rb
+++ record_closet_api/app/controllers/users_controller.rb
@@ -1,23 +1,51 @@
 class UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
 
-    def profile
-        render json: { user: UserSerializer.new(current_user) }, status: :accepted
-    end
-    
-    
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-            @token = encode_token({ user_id: @user.id })
-            render json: { user: UserSerializer.new(@user) }, status: :created
+def index
+    @users = User.all
+        if @users
+            render json: {
+            users: @users
+        }
         else
-            render json: { error: 'failed to create user' }, status: :unprocessable_entity
+            render json: {
+            status: 500,
+            errors: ['no users found']
+        }
+        end
+    end
+def show
+    @user = User.find(params[:id])
+        if @user
+            render json: {
+            user: @user
+        }
+        else
+            render json: {
+            status: 500,
+            errors: ['user not found']
+        }
         end
       end
-    
-      private
-      def user_params
-        params.require(:user).permit(:username, :password)
-      end
+      
+      def create
+    @user = User.new(user_params)
+        if @user.save
+            login!  
+            render json: {
+            status: :created,
+            user: @user
+        }
+        else 
+            render json: {
+            status: 500,
+            errors: @user.errors.full_messages
+        }
+        end
+    end
+private
+      
+     def user_params
+         params.require(:user).permit(:username, :password, :password_confirmation)
+     end
 end
+
