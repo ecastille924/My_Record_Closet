b226f0bb9dbc29a42d5df9e12c0fb319b2ff3525
Erick Castille <ecastille924@gmail.com>
2021-09-26 12:01:26 -0400

    building out login form

--- record_closet_client/src/actions/UsersActions.js
+++ record_closet_client/src/actions/UsersActions.js
@@ -1,20 +1,28 @@
 export const fetchUsers = () => {
+
+    const token = localStorage.getItem("jwt")
+
     return (dispatch) => {
-        fetch('http://localhost:3000/records')
-        .then(resp => resp.json())
-        .then(users => dispatch({type: 'FETCH_USERS', payload: users}))
+        fetch('http://localhost:3000/users'),{
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        // .then(resp => resp.json())
+        // .then(users => dispatch({type: 'FETCH_USERS', payload: users}))
     }
 }
 
-export const saveUser = user => {
-    return (dispatch) => {
-        fetch('http://localhost:3000/users', {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: { 'Content-type': 'application/json'}
-        })
-        .then(resp => resp.json())
-        .then(user => dispatch({type: "SAVE_USER", payload: user}))
+// export const saveUser = user => {
+//     return (dispatch) => {
+//         fetch('http://localhost:3000/users', {
+//             method: 'POST',
+//             body: JSON.stringify(user),
+//             headers: { 'Content-type': 'application/json'}
+//         })
+//         .then(resp => resp.json())
+//         .then(user => dispatch({type: "SAVE_USER", payload: user}))
     
-    }
-}
+//     }
+// }
