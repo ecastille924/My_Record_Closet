a8796305cab3abb592cc0a8e3ad3775f1b1a3a5b
Erick Castille <ecastille924@gmail.com>
2021-09-26 12:01:45 -0400

    building out login form

--- record_closet_client/src/components/Login.js
+++ record_closet_client/src/components/Login.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "../App.css"
+import { connect } from 'react-redux'
+import { UsersActions } from '../actions/UsersActions'
 
 
-const Login = () => {
-    return(
-        <div>
-          Log in here
-        </div>
+class Login extends Component {
+  constructor(props){
+    super(props)
+
+    this.props.logout()
+
+    this.state = {
+      username: "",
+      password:"",
+      submitted: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+  handleChange(e){
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+  handleSubmit(e){
+    e.preventDefault()
+
+    this.setState({submitted: true})
+    const { username, password } = this.state
+    if (username && password) {
+      this.props.login(username, password)
+    }
+  }
+
+  render(){
+    const { loggedIn } = this.props
+    const { username, password, submitted } = this.state
+    return (
+      <form className= "login-form" onSubmit= {this.handleSubmit}>
+
+      </form>
     )
+  
+  }
 }
 
 export default Login
