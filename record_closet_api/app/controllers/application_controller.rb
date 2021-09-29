3c8ed7b12548fa937ad95f602f933aecd5259edb
Erick Castille <ecastille924@gmail.com>
2021-09-29 17:59:48 -0400

    adding conditional statements validating user based on user_ud

--- record_closet_api/app/controllers/application_controller.rb
+++ record_closet_api/app/controllers/application_controller.rb
@@ -9,5 +9,15 @@ class ApplicationController < ActionController::Base
             !!current_user
       end
 
-      def current_user = request.headers[:Authorization]
+      def current_user 
+            authorization_header = request.headers[:Authorization]
+            if authorization_header
+                  user_token = authorization_header.split(" ")[1]
+                  begin
+                        @user_id = JWT.decode(user_token, Rails.application.secrets.secret_key_base[0])[0]["user_id"]
+                  rescue JWT::DecodeError
+                        nil
+                  end
+            end
+      end
 end
