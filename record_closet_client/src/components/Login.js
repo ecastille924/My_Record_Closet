059745f4892deabf33fc22d7ea0b8dc0503a6a39
Erick Castille <ecastille924@gmail.com>
2021-09-29 17:23:11 -0400

    creating login method in app.js

--- record_closet_client/src/components/Login.js
+++ record_closet_client/src/components/Login.js
@@ -18,11 +18,16 @@ handleChange = (e) => {
   })
 }
 
+handleSubmit = (e) => {
+  e.preventDefault()
+  this.props.login(this.state)
+}
+
 render(){
   return (
     <div>
       Login:
-      <form className= "signup-form">
+      <form className= "signup-form" onSubmit={this.handleSubmit}>
         <input name='username' type="text" value={this.state.username} onChange={this.handleChange} placeholder="username"/>
       <br/>
         <input name='password' type="password" value={this.state.password} onChange={this.handleChange} placeholder="password"/>
